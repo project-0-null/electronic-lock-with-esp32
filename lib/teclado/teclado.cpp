c11d95a70caf9a2425146e849c1224c0b77f8a46
project-0-null <miguelmagal0802@gmail.com>
2026-02-04 10:55:20 -0300

    consertando a biblioteca do teclado matrcial

--- lib/teclado/teclado.cpp
+++ lib/teclado/teclado.cpp
@@ -1,62 +1,57 @@
-#include <Arduino.h>
 #include "teclado.h"
-//==================================================
-// configuração do teclado matricial 4x4
-//==================================================
 
-#define linha1 48
-#define linha2 45
-#define linha3 0 
-#define linha4 35
-
-#define coluna1 36
-#define coluna2 37
-#define coluna3 38
-#define coluna4 39
-
-char teclas[4][4] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
-
-int linhas[] = {linha1, linha2, linha3, linha4};
-int colunas[] = {coluna1, coluna2, coluna3, coluna4};
-
-unsigned long ultimoTempo[4][4] = {0};
-bool ultimoEstado[4][4] = {false};
-
-//=================================================
-//interrupçao de tempo
-//=================================================
-hw_timer_t * timer = NULL;
-bool flag;
-
-volatile char teclaPressionada = '\0';
-volatile bool teclaNova = false;
-
-volatile unsigned long ultimo_interrupçao = 0;
-#define DEBOUNCE_TIME 50
-
-//----->função de interrupção do timer<-----
-void IRAM_ATTR onTimer(){
-    flag = true;
-    //timerAlarm
-}
-void setup(){
-timer=timerBegin(0,80,true);
-timerAttachInterrupt(timer, &onTimer, true);
-}//???????????????????????????????????????? tem que verificar essa merda
-
-
+//controle para verificação para o debounce
 
 
 //==================================================
 //FUNÇÕES DO TECLADO
 //==================================================
 
-bool DetectaTecla(int linhaIDX, int colunaIDX){
+void teclado_init(){
+    //energiza as colunas
+    for (int i =0;i<NUM_COLUNAS;i++){
+        pinMode(colunas[i], LOW);
+    }
+    //configura as lnhas como entrada
+    for(int i=0;i<NUM_LINHAS;i++){
+        pinMode(linhas[i],INPUT);
+    }
+
+    //inicializa e/ou limpa os arrays de controle 
+    for (int i =0; i<NUM_LINHAS; i++){
+        for(int j =0;j<NUM_COLUNAS;j++){
+            ultimoTempo[i][i]=0;
+            ultimoEstado[i][i]=false;
+        }
+    }
+    //configura a varredura (10ms = 100hz)
+    //alocaçao 0, 80(1Mhz), crescente
+    timer= timerBegin(0,80,true);
+    //interrupção
+    timerAttachInterrupt(timer, &onTimer, true);
+    //define o periodo -> 10000us = 10ms
+    timerAlarmWrite(timer,10000,true);//aparentemente esse true significa auto-reload que eu acho que é ficar se armando sozinho
+    //adivinha oq esse aqui faz
+    timerAlarmEnable(timer);
+}
+
+void teclado_varredura(){
+    //essa função aqui energiza as colunas e depois desenergiza, e ai usa a função detecta tecla pra verificar se foi pressionada(se teve saida na linha)
+    for (int col=0; col < NUM_COLUNAS;col++){
+        digitalWrite(colunas[col],HIGH);
+        // delayMicroseconds(10);
+        for (int lin=0; lin<NUM_LINHAS;lin++){
+            if( DetectaTecla(lin,col)){
+                ultimaTeclaPressionada = teclas[lin][col];
+                tecla_disponivel = true;
+            }
+        }
+        digitalWrite(colunas[col],LOW);
+    }
+}
+
+static bool DetectaTecla(int linhaIDX, int colunaIDX){
+    //essa aqui faz debounce e detecta pressionamento
     bool estado_atual = digitalRead(linhas[linhaIDX]);
     unsigned long now = millis(); //pega o tempo atual
     bool pressionada = false;
@@ -68,4 +63,20 @@ bool DetectaTecla(int linhaIDX, int colunaIDX){
         ultimoEstado[linhaIDX][colunaIDX] = estado_atual;
         ultimoTempo[linhaIDX][colunaIDX] = now;
     }
+    return pressionada;
+}
+
+static void IRAM_ATTR onTimer(){
+    teclado_varredura();
+}
+
+char ler_teclado(){
+    if(tecla_disponivel){
+        char tecla = ultimaTeclaPressionada;
+        //limpa apos leitura
+        ultimaTeclaPressionada= '\0';
+        tecla_disponivel=false;
+        return tecla;
+    }
+    return '\0';//nenhuma tecla disponivel, nao retorna nada uai :|
 }
---
+++ lib/teclado/teclado.h
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+
+#define DEBOUNCE_MS 50
+#define NUM_LINHAS 4
+#define NUM_COLUNAS 4
+
+//==================================================
+// configuração do teclado matricial 4x4
+//==================================================
+#define linha1 48
+#define linha2 45
+#define linha3 0 
+#define linha4 35
+
+#define coluna1 36
+#define coluna2 37
+#define coluna3 38
+#define coluna4 39
+
+const int linhas[] = {linha1, linha2, linha3, linha4};
+const int colunas[] = {coluna1, coluna2, coluna3, coluna4};
+
+
+const char teclas[NUM_LINHAS][NUM_COLUNAS] = {//coloquei assim pra poder verificar tanto no woki tanto quando na montagem do lab
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+unsigned long ultimoTempo[NUM_LINHAS][NUM_COLUNAS] = {0};
+bool ultimoEstado[NUM_LINHAS][NUM_COLUNAS] = {false};
+
+static hw_timer_t * timer = NULL;
+
+//controle do teclado
+volatile char ultimaTeclaPressionada = '\0';
+volatile bool tecla_disponivel = false;
+
+volatile unsigned long ultimo_interrupçao = 0;
+
+
+//=================================================
+// declaração de funções
+//=================================================
+
+
+static bool DetectaTecla(int linhaIDX, int colunaIDX);
+static void IRAM_ATTR onTimer();
+void teclado_varredura();
+void inicializa_teclado(){}
+char ler_teclado();
